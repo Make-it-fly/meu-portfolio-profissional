987baaa45c620bd4595216d7a0af5d0e43e68f91
Lucas Magalhães <lucoder618@gmail.com>
2023-03-10 10:08:41 -0300

    feat: full stack att

--- src/pages/HoIAm/index.jsx
+++ src/pages/HoIAm/index.jsx
@@ -1,97 +1,128 @@
 import ContentTemplate from "../../components/ContentTemplate/index";
-import myPicture from "../../images/myself.jpg"
+import myPicture from "../../images/myself.jpg";
 import StyledPageHoIAm from "./style";
 
-import { BsFillBriefcaseFill } from 'react-icons/bs'
+import { BsFillBriefcaseFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import { useLanguageContext } from "../../contexts/languageContext";
 
-export default function PageHoIAm(){
-  const {isPortuguese} = useLanguageContext()
-  return (
-    <ContentTemplate>
-      <StyledPageHoIAm>
-        <div className="content-container">
-          <section className="profile">
-            <img src={myPicture} alt="Foto de perfil do Lucas Magalhães"></img>
-            <div className="description">
-              <h2>Lucas Magalhães</h2>
-              <p>
-                <BsFillBriefcaseFill/>
-                Front end - Web Developer
-              </p>
-            </div>
-          </section>
-          <section className="hard-skills">
-            <h2>Hard Skills</h2>
-            <ul className="skills-container">
-              <li className="skill-card">
-                <h3>{isPortuguese ? "Tecnologias" : "Technologies"}</h3>
-                <ul>
-                  <li>HTML5</li>
-                  <li>CSS3</li>
-                  <li>JavaScript</li>
-                  <li>TypeScript</li>
-                  <li>Express.js</li>
-                  <li>PostgreSQL</li>
-                </ul>
-              </li>
-              <li className="skill-card">
-                <h3>{isPortuguese ? "Bibliotecas" : "Libraries"}</h3>
-                <ul>
-                  <li>React</li>
-                  <li>React Icons</li>
-                  <li>React Router Dom</li>
-                  <li>React Hook Form</li>
-                  <li>Yup</li>
-                  <li>Styled Components</li>
-                  <li>React Calendar</li>
-                  <li>Dotenv</li>
-                  <li>Sucrase</li>
-                  <li>Nodemon</li>
-                  <li>Jsonwebtoken</li>
-                  <li>Uuid</li>
-                  <li>Bcryptjs</li>
-                </ul>
-              </li>
-              <li className="skill-card">
-                <h3>{isPortuguese ? "Design e Marketing" : "Design and Marketing"} </h3>
-                <ul>
-                  <li>FIGMA</li>
-                  <li>Adobe Photoshop</li>
-                  <li>Adobe Premiere</li>
-                  <li>Facebook Ads</li>
-                  <li>Google Ads</li>
-                </ul>
-              </li>
-            </ul>
-          </section>
-          <section className="soft-skills">
-            <h2>Soft Skills</h2>
-            <div className="text-content">
-              <h3>{isPortuguese ? "Inteligência Emocional" : "Emotional Intelligence"} </h3>
-              <p>{isPortuguese ? "Minha trajetória profissional foi toda construída em cima de experiências conquistadas com esforço, garra e disciplina. Tive um contato profundo com Marketing digital, Diagramação de livros e revistas como freelancer, negociação, prospecção de clientes e pressão em ambientes de trabalho. Me deparei com muitas situações onde eu tive que manter minha perseverança e minha fé intactas. Isso me permitiu desenvolver resiliência." 
-              : 
-              "My professional trajectory was all built on top of experiences gained with effort, determination and discipline. I had a deep contact with digital marketing, layout of books and magazines as a freelancer, negotiation, prospecting for clients and pressure in work environments. I came across many situations where I had to keep my faith. It allowed me to develop resilience."} 
-              </p>
-            </div>
-          </section>
-          <section className="sobre-mim">
-              <h2>{isPortuguese ? "Sobre mim" : "About me"} </h2>
-              <div className="text-content">
-                <h3>{isPortuguese ? "Minha paixão" : "My Passion"} </h3>
-                <p>{isPortuguese ? "Na Kenzie Academy Brasil tenho tido a oportunidade de desenvolver diversos projetos. Adquiri uma grande paixão pela área de desenvolvimento. Amo programar e aprender coisas novas." 
-                : 
-                "At Kenzie Academy Brasil I'm having the opportunity to develop several projects. I acquired a great passion for the development area. I love programming and learning new things."}</p>
-              </div>
-              <div className="text-content">
-                <h3>{isPortuguese ? "Crença profissional" : "Professional Belief"} </h3>
-                <p>{isPortuguese ? "Todo ser humano é capaz de adquirir qualquer conhecimento. A única variável é o tempo que cada um precisa investir." : "Every human being is capable of acquiring any knowledge. The only variable is the time each one needs to invest."} </p>
-              </div>
-          </section>
-        </div>
-        <Link to={"/contato"} className="button-default">{isPortuguese ? "Entre em contato" : "Contact me"} </Link>
-      </StyledPageHoIAm>
-    </ContentTemplate>
-  )
+export default function PageHoIAm() {
+	const { isPortuguese } = useLanguageContext();
+	return (
+		<ContentTemplate>
+			<StyledPageHoIAm>
+				<div className="content-container">
+					<section className="profile">
+						<img
+							src={myPicture}
+							alt="Foto de perfil do Lucas Magalhães"></img>
+						<div className="description">
+							<h2>Lucas Magalhães</h2>
+							<p>
+								<BsFillBriefcaseFill />
+								Full Stack - Developer
+							</p>
+						</div>
+					</section>
+					<section className="hard-skills">
+						<h2>Hard Skills</h2>
+						<ul className="skills-container">
+							<li className="skill-card">
+								<h3>
+									{isPortuguese
+										? "Tecnologias"
+										: "Technologies"}
+								</h3>
+								<ul>
+									<li>HTML5</li>
+									<li>CSS3</li>
+									<li>JavaScript</li>
+									<li>TypeScript</li>
+									<li>Node.js</li>
+									<li>Django</li>
+									<li>PostgreSQL</li>
+								</ul>
+							</li>
+							<li className="skill-card">
+								<h3>
+									{isPortuguese ? "Bibliotecas" : "Libraries"}
+								</h3>
+								<ul>
+									<li>React</li>
+									<li>React Icons</li>
+									<li>React Router Dom</li>
+									<li>React Hook Form</li>
+									<li>Yup</li>
+									<li>Styled Components</li>
+									<li>Dotenv</li>
+									<li>Sucrase</li>
+									<li>Nodemon</li>
+									<li>Jsonwebtoken</li>
+									<li>Uuid</li>
+									<li>Bcryptjs</li>
+								</ul>
+							</li>
+							<li className="skill-card">
+								<h3>
+									{isPortuguese
+										? "Design e Marketing"
+										: "Design and Marketing"}{" "}
+								</h3>
+								<ul>
+									<li>FIGMA</li>
+									<li>Adobe Photoshop</li>
+									<li>Adobe Premiere</li>
+									<li>Facebook Ads</li>
+									<li>Google Ads</li>
+								</ul>
+							</li>
+						</ul>
+					</section>
+					<section className="soft-skills">
+						<h2>Soft Skills</h2>
+						<div className="text-content">
+							<h3>
+								{isPortuguese
+									? "Inteligência Emocional"
+									: "Emotional Intelligence"}{" "}
+							</h3>
+							<p>
+								{isPortuguese
+									? "Minha trajetória profissional foi toda construída em cima de experiências conquistadas com esforço, garra e disciplina. Tive um contato profundo com Marketing digital, Diagramação de livros e revistas como freelancer, negociação, prospecção de clientes e pressão em ambientes de trabalho. Me deparei com muitas situações onde eu tive que manter minha perseverança e minha fé intactas. Isso me permitiu desenvolver resiliência."
+									: "My professional trajectory was all built on top of experiences gained with effort, determination and discipline. I had a deep contact with digital marketing, layout of books and magazines as a freelancer, negotiation, prospecting for clients and pressure in work environments. I came across many situations where I had to keep my faith. It allowed me to develop resilience."}
+							</p>
+						</div>
+					</section>
+					<section className="sobre-mim">
+						<h2>{isPortuguese ? "Sobre mim" : "About me"} </h2>
+						<div className="text-content">
+							<h3>
+								{isPortuguese ? "Minha paixão" : "My Passion"}{" "}
+							</h3>
+							<p>
+								{isPortuguese
+									? "Na Kenzie Academy Brasil tenho tido a oportunidade de desenvolver diversos projetos. Adquiri uma grande paixão pela área de desenvolvimento. Amo programar e aprender coisas novas."
+									: "At Kenzie Academy Brasil I'm having the opportunity to develop several projects. I acquired a great passion for the development area. I love programming and learning new things."}
+							</p>
+						</div>
+						<div className="text-content">
+							<h3>
+								{isPortuguese
+									? "Crença profissional"
+									: "Professional Belief"}{" "}
+							</h3>
+							<p>
+								{isPortuguese
+									? "Todo ser humano é capaz de adquirir qualquer conhecimento. A única variável é o tempo que cada um precisa investir."
+									: "Every human being is capable of acquiring any knowledge. The only variable is the time each one needs to invest."}{" "}
+							</p>
+						</div>
+					</section>
+				</div>
+				<Link to={"/contato"} className="button-default">
+					{isPortuguese ? "Entre em contato" : "Contact me"}{" "}
+				</Link>
+			</StyledPageHoIAm>
+		</ContentTemplate>
+	);
 }
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,43 +1,70 @@
-import { Link } from "react-router-dom"
-import StyledPageHomePage from "./style"
-import { FaFileDownload } from 'react-icons/fa';
-import { AiFillLinkedin } from 'react-icons/ai';
-import { BsGithub } from 'react-icons/bs';
-import curriculum_PtBR from '../../documents/Currículo Front End.pdf'
-import curriculum_EnUs from '../../documents/Currículo Front End EN-US.pdf'
+import { Link } from "react-router-dom";
+import StyledPageHomePage from "./style";
+import { FaFileDownload } from "react-icons/fa";
+import { AiFillLinkedin } from "react-icons/ai";
+import { BsGithub } from "react-icons/bs";
+import curriculum_PtBR from "../../documents/Currículo Front End.pdf";
+import curriculum_EnUs from "../../documents/Currículo Front End EN-US.pdf";
 import { useLanguageContext } from "../../contexts/languageContext";
 
+export default function PageHomePage() {
+	const { isPortuguese } = useLanguageContext();
 
-export default function PageHomePage(){
-  const {isPortuguese} = useLanguageContext()
-
-  return (
-    <StyledPageHomePage>
-      <div className="container">
-        <div className="content">
-          <h1>
-            {isPortuguese ? "Olá" : "Hello!"}
-          </h1>
-          <h2>{isPortuguese ? "Eu sou Lucas Magalhães" : "I am Lucas Magalhães"}</h2>
-          <p>
-            {isPortuguese ? 
-          "Desenvolvedor Front End e apaixonado por tecnologia!" : 
-          "Front End Developer and in passion with technology!"}
-          </p>
-          <ul className="social-media-buttons">
-            <li><a href={"https://www.linkedin.com/in/lucas-magalh%C3%A3es-3b034120a/"} target="_blank" rel="noreferrer"><AiFillLinkedin/></a></li>
-            <li><a href={"https://github.com/Make-it-fly"} target="_blank" rel="noreferrer"><BsGithub/></a></li>
-          </ul>
-          <div className="content-buttons">
-            <Link to={"/meus-projetos"} className="button-white"> 
-              {isPortuguese ? "Veja meus projetos!" : "Look at my projects!"}
-            </Link>          
-            <a className="button-red" href={isPortuguese ? curriculum_PtBR : curriculum_EnUs} download="Lucas Magalhães - Front End Curriculum"><FaFileDownload/> 
-              {isPortuguese ? "Currículo Download" : "Download Curriculum"}
-            </a>
-          </div>
-        </div>
-      </div>
-    </StyledPageHomePage>
-  )
+	return (
+		<StyledPageHomePage>
+			<div className="container">
+				<div className="content">
+					<h1>{isPortuguese ? "Olá" : "Hello!"}</h1>
+					<h2>
+						{isPortuguese
+							? "Eu sou Lucas Magalhães"
+							: "I am Lucas Magalhães"}
+					</h2>
+					<p>
+						{isPortuguese
+							? "Desenvolvedor Full Stack e apaixonado por tecnologia!"
+							: "Full Stack Developer and in passion with technology!"}
+					</p>
+					<ul className="social-media-buttons">
+						<li>
+							<a
+								href={
+									"https://www.linkedin.com/in/lucas-magalh%C3%A3es-3b034120a/"
+								}
+								target="_blank"
+								rel="noreferrer">
+								<AiFillLinkedin />
+							</a>
+						</li>
+						<li>
+							<a
+								href={"https://github.com/Make-it-fly"}
+								target="_blank"
+								rel="noreferrer">
+								<BsGithub />
+							</a>
+						</li>
+					</ul>
+					<div className="content-buttons">
+						<Link to={"/meus-projetos"} className="button-white">
+							{isPortuguese
+								? "Veja meus projetos!"
+								: "Look at my projects!"}
+						</Link>
+						<a
+							className="button-red"
+							href={
+								isPortuguese ? curriculum_PtBR : curriculum_EnUs
+							}
+							download="Lucas Magalhães - Front End Curriculum">
+							<FaFileDownload />
+							{isPortuguese
+								? "Currículo Download"
+								: "Download Curriculum"}
+						</a>
+					</div>
+				</div>
+			</div>
+		</StyledPageHomePage>
+	);
 }
